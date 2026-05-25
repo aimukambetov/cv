29c9640a6cc9889d929cb5c431e31675c59037cf
Sanatzhan Aimukambetov <aimukambetov@kolesa.kz>
2026-05-25 14:39:49 +0500

    Remove icon-button, LinkedIn label, speech section

--- script.js
+++ script.js
@@ -242,14 +242,9 @@ function setLang(lang) {
 }
 
 const downloadButton = document.querySelector(".bottom-button");
-const profileButton = document.querySelector(".icon-button");
 
 downloadButton?.addEventListener("click", () => {
   window.print();
-});
-
-profileButton?.addEventListener("click", () => {
-  document.querySelector(".contact-pills")?.scrollIntoView({ behavior: "smooth" });
 });
 
 // Language toggle
